9ee49e13da710faa98c3245db5aad9c2264c6f27
Gilmar F <gilmar74796@gmail.com>
2020-10-16 12:48:50 -0300

    Aula 1

---
+++ RedDiamond-teste/redcontrol/src/test/java/rediamond/redcontrol/RedControlApplicationTests.java
@@ -0,0 +1,13 @@
+package rediamond.redcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
