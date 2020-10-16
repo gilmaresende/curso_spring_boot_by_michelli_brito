8097722dd4cf6a0498e9a5a8a209994d5fbe3be1
Gilmar F <gilmar74796@gmail.com>
2020-10-16 13:04:46 -0300

    Aula 2

--- RedDiamond-teste/redcontrol/src/main/java/rediamond/redcontrol/controllers/IndexController.java
+++ RedDiamond-teste/redcontrol/src/main/java/rediamond/redcontrol/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package rediamond.redcontrol;
+package rediamond.redcontrol.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ RedDiamond-teste/redcontrol/src/main/java/rediamond/redcontrol/models/Evento.java
@@ -0,0 +1,19 @@
+package rediamond.redcontrol.models;
+
+public class Evento {
+
+    private String nome;
+    private String local;
+    private String data;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String horario;
+
+}
