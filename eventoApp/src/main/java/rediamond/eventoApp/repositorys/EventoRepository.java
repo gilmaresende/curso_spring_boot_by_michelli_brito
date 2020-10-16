eac59c6a555268f6954356824dcd1d928ea182b7
Gilmar F <gilmar74796@gmail.com>
2020-10-16 18:48:20 -0300

    Aula 4
    Curso Spring Boot aula 04: Salvando dados do formulário no banco de dados
    https://www.youtube.com/watch?v=miN5ygzFhtI&list=PL8iIphQOyG-DHLpEx1TPItqJamy08fs1D&index=4

---
+++ eventoApp/src/main/java/rediamond/eventoApp/repositorys/EventoRepository.java
@@ -0,0 +1,7 @@
+package rediamond.eventoApp.repositorys;
+
+import org.springframework.data.repository.CrudRepository;
+import rediamond.eventoApp.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String> {
+}
