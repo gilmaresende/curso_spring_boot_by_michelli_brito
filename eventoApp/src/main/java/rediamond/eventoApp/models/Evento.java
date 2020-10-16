05625b32cd5e54413320f5dc364e673606fec76b
Gilmar F <gilmar74796@gmail.com>
2020-10-16 18:35:34 -0300

    Aula 3
    Curso Spring Boot aula 03: Criando e configurando um banco de dados MySQL
    https://www.youtube.com/watch?v=FHRYijYhJYA&list=PL8iIphQOyG-DHLpEx1TPItqJamy08fs1D&index=3

--- eventoApp/src/main/java/rediamond/eventoApp/models/Evento.java
+++ eventoApp/src/main/java/rediamond/eventoApp/models/Evento.java
@@ -1,10 +1,33 @@
 package rediamond.eventoApp.models;
 
-public class Evento {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Evento implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long codigo;
 
     private String nome;
     private String local;
     private String data;
+
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
 
     public String getNome() {
         return nome;
